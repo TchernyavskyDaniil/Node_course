db590c56c587ab1dee2b273314e18b6188beb483
Tchernyavsky <tchernyavsky.daniil@yandex.ru>
2017-12-18 11:01:55 +0300

    ex2

--- main.js
+++ main.js
@@ -1,72 +1,32 @@
-const path = require('path');
-const fs = require('fs');
-const rimraf = require('rimraf');
+const http = require('http');
+const Events = require('events');
+const eventEmmiter = new Events ();
 
-const base = './old';
-const newFolders = './new';
+const PORT = process.env.PORT || 3000;
+const INTERVAL = process.env.INTERVAL || 1000;
+const TIMEOUT = process.env.TIMEOUT || 10000;
 
-const readDir = (base, level) => {
-    if (!fs.existsSync('./new')) {
-        fs.mkdirSync('./new');
-    }
+eventEmmiter.on('connected', () => {
+    console.log("HELLO");
+});
 
-    // Проверка если в папке что - то лежит
-    let files = fs.readdirSync(base);
-    let folders = fs.readdirSync(newFolders);
 
-    files.forEach( item => {
-        let localBase = path.join(base, item);
-        // console.log(localBase);
-        let state = fs.statSync(localBase);
-        if (state.isDirectory()) {
-            readDir(localBase, level + 1);
-        } else {
-            let mas = item.split('.');
-            let type = mas[mas.length -1];
+http.createServer(function (request, response) {
+    eventEmmiter.emit('connected');
 
-            // Если такой папки нет, то создаем
-            if (!folders.includes(type)) {
-                folders.push(type);
-                fs.mkdirSync(newFolders + '/' + type);
-            }
+    // Задаем интервал вывода времени/даты UTC
+    var timerId = setInterval(() => {
+        let date = new Date().toUTCString();
+        response.write(date + '\n');
+        console.log(date);
+    }, INTERVAL);
 
-            let filePath = base + '/' + item;
-            fs.readFile(filePath, (err, data) => {
-                if (err) {
-                    console.log(err.message);
-                }
+    // Объявляем прекращение
+    setTimeout(() => {
+        clearInterval(timerId);
+        console.log("STOP");
+        response.end();
+    }, TIMEOUT);
+}).listen(PORT);
 
-                fs.writeFile(newFolders + '/' + type + '/' + item, data, err => {
-                   if (err) {
-                       console.log(err.message);
-                   }
-                });
 
-                // Выбрали способ глупца через rimraf
-                // Удаляем все наши расширения
-                // fs.unlink(filePath, err => {
-                //     if (err) {
-                //         console.log(err.message);
-                //     }
-                // });
-            });
-        }
-
-        // Нативно не вышло, я выбрал способ глупца rimraf
-        // files.forEach(dir => {
-        //     fs.rmdir(localBase, err => {
-        //         if (err) {
-        //             console.log(err.message);
-        //         }
-        //     });
-        // });
-
-        rimraf('old', err => {
-            if (err) {
-                console.log(err.message);
-            }
-        });
-    });
-};
-
-readDir(base, 0);
