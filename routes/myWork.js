16bbd45504a660eb7862d6d299300016ceccf83b
Tchernyavsky <tchernyavsky.daniil@yandex.ru>
2017-12-23 20:23:54 +0300

    hot-fix

--- routes/myWork.js
+++ routes/myWork.js
@@ -59,7 +59,7 @@ router.post('/', (req, res, next) => {
             }
 
             let dir = fileName.substr(fileName.indexOf('upload'));
-            db.set(fields.name, {
+            db.set(fields.projectName, {
                 'file' : dir,
                 'projectName':fields.projectName,
                 'projectUrl':fields.projectUrl,
