fdef0ba8c2b52b8741632e8b4e0eab8814bdab2b
Tchernyavsky <tchernyavsky.daniil@yandex.ru>
2017-12-28 15:28:35 +0300

    test

---
+++ routes/test.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/saveNewUser', function(req, res) {
+
+});
+
+router.post('/login', function (req, res) {
+
+});
+
+router.post('/authFromToken', function(req, res) {
+
+});
+
+router.put('/updateUser/:id', function(req, res) {
+
+});
+
+router.delete('/deleteUser/:id', function(req, res) {
+
+});
+
+router.post('/saveUserImage/:id', function(req, res) {
+
+});
+
+router.get('/getNews', function(req, res) {
+
+});
+
+router.post('/newNews', function(req, res) {
+
+});
+
+router.put('/updateNews/:id', function(req, res) {
+
+});
+
+router.delete('/deleteNews/:id', function(req, res) {
+
+});
+
+router.get('/getUsers', function(req, res) {
+
+});
+
+router.put('/updateUserPermission/:id', function(req, res) {
+
+});
+
+module.exports = router;
